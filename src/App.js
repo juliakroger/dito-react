18fbba22a2256f909973a6ae061972ef23a7785b
Julia <julia.kroger@gmail.com>
2019-04-25 21:50:48 -0300

    Refatoring the data to use in timeline

--- src/App.js
+++ src/App.js
@@ -3,9 +3,38 @@ import axios from 'axios';
 import Header from './Components/Header';
 
 class App extends Component {
+  state = {
+    purchase: null,
+  };
+
+  compare = (first, second) => {
+    const firstTimestamp = first.compra.timestamp;
+    const secondTimestamp = second.compra.timestamp;
+    if (firstTimestamp > secondTimestamp)  return 1;
+    else if (firstTimestamp < secondTimestamp)  return -1;
+    else return 0;
+  };
+
+  RefactorData = (data) => {
+    let ShoppingPurchase = [];
+    let ProductPurchase = [];
+    data.forEach(event => {
+      if (event.event === 'comprou-produto') ProductPurchase.push(event);
+      else if (event.event === 'comprou') ShoppingPurchase.push({compra:event, produtos:[]});
+    });
+    ShoppingPurchase.forEach(shopping => {
+      ProductPurchase.forEach(product => {
+        if (product.timestamp.slice(0,10) === shopping.compra.timestamp.slice(0,10)) shopping.produtos.push(product)
+      })
+    });
+    ShoppingPurchase.sort(this.compare);
+    console.log(ShoppingPurchase)
+    this.setState({purchase: ShoppingPurchase})
+  };
+
   componentWillMount() {
     axios.get('https://storage.googleapis.com/dito-questions/events.json')
-        .then(res => console.log(res))
+        .then(res => this.RefactorData(res.data.events))
         .catch(error => console.log(error))
   }
 
