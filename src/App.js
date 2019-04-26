2a4cfaf0e35a31da0068c28ab23a162f2feb75ca
Julia <julia.kroger@gmail.com>
2019-04-25 21:35:10 -0300

    adding axios and the API call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React, {Component} from 'react';
+import axios from 'axios';
+
 
 class App extends Component {
+  componentWillMount() {
+    axios.get('https://storage.googleapis.com/dito-questions/events.json')
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
+  }
+
   render(){
     return (
         <div>
